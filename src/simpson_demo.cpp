94c1a63c2406cf1191a86a78ff3eb6d87a14dbb4
Filipe <filiperodrigues97@gmail.com>
2019-12-05 15:38:16 -0300

    Pra fessora

--- src/simpson_demo.cpp
+++ src/simpson_demo.cpp
@@ -6,6 +6,9 @@
 
 using namespace std;
 
+ifstream menuFile("menu.txt");
+ifstream splashFile("splash.txt");
+
 struct Configuration {
 	int iterations;
 	int processes;
@@ -27,6 +30,24 @@ struct Configuration {
 	
 };
 
+int readInteger () {
+	string input = "";
+	int valor;
+	do {
+		cout << "Please enter a valid number: ";
+		getline(cin, input);
+
+		// This code converts from string to number safely.
+		stringstream myStream(input);
+		if (myStream >> valor) break;
+		cout << "Invalid number, please try again" << endl;
+		cin.clear();
+		cin.sync();
+		cin.ignore(1000);
+	} while (true);
+	return valor;
+}
+
 string brewFileName(integer k) {
 	string fileName = "K_" + to_string(k) + ".txt";
 	return fileName;
@@ -39,53 +60,56 @@ void clear () {
 
 void pressToContinue() {
 	string lixo;
-	cin >> lixo;
-	cin.ignore(10000, '\n');
+	getline(cin, lixo);
 }
 
 void printSplash() {
 	clear();
-	ifstream input("splash.txt");
 	string line;
-	while (input.good()) {
-		getline(input, line);
+	while (splashFile.good()) {
+		getline(splashFile, line);
 		cout << line << endl;
 	}
-	input.close();
+	menuFile.clear();
+	menuFile.seekg(0, ios::beg);
 	pressToContinue();
 }
 
 void printMenu() {
 	clear();
-	ifstream input("menu.txt");
 	string line;
-	while (input.good()) {
-		getline(input, line);
+	while (menuFile.good()) {
+		getline(menuFile, line);
 		cout << line << endl;
 	}
-	input.close();
+	menuFile.clear();
+	menuFile.seekg(0, ios::beg);
+}
+
+void runInstance(int procs, int iterations, string file, double a, double b, integer k, int precision) {
+	stringstream ss;
+	ss.str("");
+	ss << "mpiexec -np " << ((procs == 0) ? (1) : (procs)) << " ";
+	ss << "./parallel38 --performance-test " << iterations << " ";
+	ss << "--file " << file << " ";
+	ss << "-a " << a << " ";
+	ss << "-b " << b << " ";
+	ss << "-k " << k << " ";
+	ss << "--precision " << precision;
+	system (ss.str().c_str());
 }
 
 void testSeries(Configuration& conf) {
 	integer k = conf.k;
 	integer step = conf.step;
 	integer max = k * 10;
-	stringstream ss;
 
 	for (k; k <= max; k += step) {
 		string file = brewFileName(k);
 		cout << endl << "Now performing series of tests with K = " << k << "...";
 		for (int procs = 0; procs <= conf.processes; procs += 4) {
-			ss.str("");
-			ss << "mpiexec -np " << ((procs == 0) ? (1) : (procs)) << " ";
-			ss << "./parallel38 --performance-test " << conf.iterations << " ";
-			ss << "--file " << file << " ";
-			ss << "-a " << conf.a << " ";
-			ss << "-b " << conf.b << " ";
-			ss << "-k " << k << " ";
-			ss << "--precision " << conf.precision;
 			cout << endl << "\t...using " << ((procs == 0) ? (1) : (procs)) << " processes";
-			system (ss.str().c_str());
+			runInstance(procs, conf.iterations, file, conf.a, conf.b, k, conf.precision);
 		}
 		cout << endl;
 	}
@@ -100,12 +124,15 @@ int main() {
 	do {
 		printMenu();
 		cout << "    > ";
-		cin >> option;
-		cin.ignore(10000, '\n');
+		option = readInteger();
 		switch (option) {
 			case 1:
 				clear();
-				system("mpirun -np 4 ./simpson38");
+				cout << "\nRunning a test with default config... ";
+				runInstance(0, conf.iterations, "standalone.txt", conf.a, conf.b, conf.k, conf.precision);
+				cout << "DONE!\n\nCheck your results in a generated text file at output/ directory!\n\n"
+					 << "Press any key to continue...";
+				pressToContinue();
 				break;
 			case 2:
 
@@ -118,5 +145,7 @@ int main() {
 				break;
 		}
 	} while (option != 0);
+	menuFile.close();
+	splashFile.close();
 	return 0;
 }
