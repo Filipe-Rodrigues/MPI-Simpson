7896f98f1f2f8f7a0ca1c50be87b9e91c701e53e
Filipe Rodrigues <filiperodrigues97@gmail.com>
2019-12-05 02:22:46 -0300

    Finished generating results, now Debugging minor features

--- src/stat_generator.cpp
+++ src/stat_generator.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+double* sequentialTimes = new double[10];
+
 string brewFileName(int proc, integer k) {
 	string fileName = "PAR_" + to_string((proc == 0) ? (1) : (proc)) + "_K_" + to_string(k) + ".txt";
 	return fileName;
@@ -11,22 +13,32 @@ string brewFileName(int proc, integer k) {
 int main() {
 	int k = 100000;
 	ofstream outputTime("output/ALL_TIMES.txt");
-	ofstream outputAcc("output/ALL_ACCURACIES.txt");
+	ofstream outputSpdUp("output/ALL_SPEEDUPS.txt");
+	ofstream outputEff("output/ALL_EFFICIENCIES.txt");
+
 	for (k; k <= 1000000; k += 100000) {
 		int proc = 0;
 		for (proc; proc <= 16; proc += 4) {
 			string file = "output/" + brewFileName(proc, k);
 			ifstream input(file.c_str());
-			double time, acc;
+			double time;
 			input >> time;
-			input >> acc;
 			input.close();
 			outputTime << time << "\t";
-			outputAcc << setprecision(16) << fixed << acc << "\t";
+			if (proc == 0) {
+				sequentialTimes[k / 100000] = time;
+			} else {
+				double speedUp = sequentialTimes[k / 100000] / time;
+				double efficiency = speedUp / (double) proc;
+				outputSpdUp << setprecision(3) << fixed << speedUp << "\t";
+				outputEff << setprecision(3) << fixed << efficiency << "\t";
+			}
 		}
 		outputTime << "\n";
-		outputAcc << "\n";
+		outputSpdUp << "\n";
+		outputEff << "\n";
 	}
 	outputTime.close();
-	outputAcc.close();
+	outputSpdUp.close();
+	outputEff.close();
 }
